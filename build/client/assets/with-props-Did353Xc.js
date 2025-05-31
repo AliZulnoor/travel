a65dfd77265ec8ec78d962ab69a157eccd840573
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2025-05-31 06:35:46 +0300

    Add files via upload

---
+++ build/client/assets/with-props-Did353Xc.js
@@ -0,0 +1,5 @@
+
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="54dcbaa0-e9aa-5352-9029-023c4e2ba921")}catch(e){}}();
+import{c as p,d as t,e,f as o,a as s,g as c}from"./chunk-D4RADZKF-BxEhnJz_.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:c()};return s.createElement(a,r)}}export{m as a,i as w};
+
+//# debugId=54dcbaa0-e9aa-5352-9029-023c4e2ba921
