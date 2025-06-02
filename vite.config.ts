3269a519307209e2f03d50a317bdf0ea833a6e9e
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2025-06-02 03:17:18 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,25 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import {sentryReactRouter, type SentryReactRouterBuildOptions} from "@sentry/react-router";
+import { sentryReactRouter, type SentryReactRouterBuildOptions } from "@sentry/react-router";
 
 const sentryConfig: SentryReactRouterBuildOptions = {
   org: "js-mastery-yx",
   project: "travel-agency",
-  // An auth token is required for uploading source maps.
-  authToken: "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"
-  // ...
+  authToken: process.env.SENTRY_AUTH_TOKEN // ✅ secure way
 };
 
 export default defineConfig(config => {
   return {
-    plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), sentryReactRouter(sentryConfig, config)],
-    sentryConfig,
+    plugins: [
+      tailwindcss(),
+      tsconfigPaths(),
+      reactRouter(),
+      sentryReactRouter(sentryConfig, config)
+    ],
+    build: {
+      outDir: "build/client"
+    },
     ssr: {
       noExternal: [/@syncfusion/]
     }
