2d2541f8dd586b0d3903810664d823712f58ce27
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2025-06-03 04:38:19 +0300

    Update instrument.server.mjs

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,16 +1,9 @@
 import 'dotenv/config';
 import * as Sentry from "@sentry/react-router";
 
-// REMOVE or COMMENT THIS OUT:
-//// import { nodeProfilingIntegration } from '@sentry/profiling-node';
-
 Sentry.init({
- dsn: "https://616943f3eec66b1d5acb4783aa1baf18@o4509344740474880.ingest.de.sentry.io/4509360074391632",
- sendDefaultPii: true,
-
- // REMOVE THIS LINE TOO:
- //// integrations: [nodeProfilingIntegration()],
-
- tracesSampleRate: 1.0,
- profilesSampleRate: 1.0,
+  dsn: "your_dsn_here",
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0
 });
