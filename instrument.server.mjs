16da5f4cf74fe0f4ac57f10ee7094b4cd6836a1e
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2025-06-03 04:40:58 +0300

    Update instrument.server.mjs

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import * as Sentry from "@sentry/react-router";
 
 Sentry.init({
-  dsn: "your_dsn_here",
+  dsn: "https://616943f3eec66b1d5acb4783aa1baf18@o4509344740474880.ingest.de.sentry.io/4509360074391632",
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0
